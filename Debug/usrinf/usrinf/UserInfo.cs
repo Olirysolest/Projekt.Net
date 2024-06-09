8f8a0f86ff1928cd399d7bf5d61ff2f729c1a9ae
Olirysolest <158155409+Olirysolest@users.noreply.github.com>
2024-06-09 22:24:18 +0200

    Add files via upload
    Projekt na zaliczenie to aplikacja o funkcjonalności CCleanera albo słynnego Odkurzacza dla systemu Windows. Zamysłem było pierwotnie stworzenie prostej aplikacji do czyszczenia komputera z plików tymczasowych, śmieci, czyszczenie poprzez flush dns, oraz oczyszczania AppData. Właściwie korzystam z tego programu w swoim systemie ponieważ napisałem go samodzielnie i wiem jak działa :D. Generalnie temat jaki obejmuje ( biorąc pod uwagę całość zajęć to operacje na plikach i obsługę błędów ) ponieważ w przypadku czyszczenia danych wiąże się to z obsługą plików.

---
+++ Debug/usrinf/usrinf/UserInfo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+
+namespace usrinf 
+{
+    public class UsersInfo 
+    {
+        int iteration = 0;
+        string fileinrow = "";
+
+       
+
+        public void GetUsersProfiles()
+        {
+           
+            
+        }
+        public string SendDatatoScreen(string data,int row)
+        {
+            return row.ToString()+" "+data+"XC";
+        }
+
+      
+}
